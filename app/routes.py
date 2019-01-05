c349cfc2e14970b439deff973d819f563f910fc7
Sean Kane <spkane31@gmail.com>
2019-01-04 21:09:54 -0500

    added base.html file and added search page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  # return "Hello, World!"
+  return render_template('index.html', title='Home')
+
+@app.route('/search')
+def search():
+  results = {}
+  results['artist'] = "Beyonce"
+  results['song'] = "Lemonade"
+  return render_template('search.html', results=results)
